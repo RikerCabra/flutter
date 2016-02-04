3e4660ba653015b01253b79241bbb1a56da09455
Dan Willman <wongidool@gmail.com>
2016-02-04 01:46:15 +0000

    reverted to @username = params[:id] per tutorial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 
   def profile
     if(User.find_by_username(current_user.username))
-      @username = current_user.username
+      @username = params[:id]
     else
       redirect_to root_path, :notice=> "User not found!"
     end
